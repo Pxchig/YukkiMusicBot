240caf80702273568454a350812622a708b313dd
Pxchig <97571119+Pxchig@users.noreply.github.com>
2022-02-07 12:35:43 +0530

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -165,7 +165,7 @@ async def play(_, message: Message):
                 message.from_user.first_name, message.from_user.id, "abcd"
             )
             await message.reply_photo(
-                photo="Utils/Playlist.jpg",
+                photo="https://telegra.ph/file/b00fdda44862ef7ebac54.jpg",
                 caption=(
                     "**Usage:** /play [Music Name or Youtube Link or Reply to Audio]\n\nIf you want to play Playlists! Select the one from Below."
                 ),
